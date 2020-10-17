4fe648967a588f2476a88b4b68c0f4c0d69ffd47
Chris Kite <chris.kat82@googlemail.com>
2020-10-17 19:29:11 +0100

    Initial Commit

---
+++ src/pet_shop.py
@@ -0,0 +1,7 @@
+# WRITE YOUR FUNCTIONS HERE
+    # def test_pet_shop_name(self):
+    #     name = get_pet_shop_name(self.cc_pet_shop)
+    #     self.assertEqual("Camelot of Pets", name)
+
+def pet_shop_name(self):
+    return name("name")
