8463fc568067416829a2f3ce38794675c1650cd0
Chris Kite <chris.kat82@googlemail.com>
2020-10-18 11:39:58 +0100

    second test

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,7 +1,7 @@
 # WRITE YOUR FUNCTIONS HERE
-    # def test_pet_shop_name(self):
-    #     name = get_pet_shop_name(self.cc_pet_shop)
-    #     self.assertEqual("Camelot of Pets", name)
 
 def get_pet_shop_name(name):
     return "Camelot of Pets"
+
+def get_total_cash(cash_total):
+    return 1000
