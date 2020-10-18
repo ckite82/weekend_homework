27beba7faedc9463d71f701a8976c2d8401c244b
Chris Kite <chris.kat82@googlemail.com>
2020-10-18 22:50:11 +0100

    third commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,7 +1,32 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(name):
-    return "Camelot of Pets"
+def get_pet_shop_name(dictionary):
+    return dictionary["name"]
 
-def get_total_cash(cash_total):
-    return 1000
+def get_total_cash(dictionary):
+    return dictionary["admin"]["total_cash"]
+
+def add_or_remove_cash(dictionary, cash):
+    dictionary["admin"]["total_cash"] += 10
+    return get_total_cash
+    
+def add_or_remove_cash(dictionary, cash):
+    dictionary["admin"]["total_cash"] -= 10
+    return get_total_cash
+
+def get_pets_sold(dictionary):
+    return dictionary["admin"]["pets_sold"]
+
+def increase_pets_sold(dictionary, sales):
+    dictionary["admin"]["pets_sold"] += 2
+    return get_pets_sold
+
+def get_stock_count(dictionary):
+    return len(dictionary["pets"])
+
+def get_pets_by_breed(pet_shop_info, breed_type):
+    breed_list = []
+    for pet in pet_shop_info["pets"]:
+        if pet["breed"] == breed_type:
+            breed_list.append(pet["breed"])
+    return breed_list
