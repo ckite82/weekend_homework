2d55ba0ccd619276737248c67099aaef79a9cccb
Chris Kite <chris.kat82@googlemail.com>
2020-10-19 17:08:00 +0100

    seventh commit, completed MVP

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -25,9 +25,9 @@ def add_or_remove_cash(dictionary, cash):
 # test 4 was same as above but subtracting, the issue i had here was I cannot get
 # both functions to run simaltaneously as the first seems to affect the total when
 # trying to run the second, how do i amend them so that they can both run.
-def add_or_remove_cash(dictionary, cash):
-    dictionary["admin"]["total_cash"] -= cash
-    return get_total_cash
+# def add_or_remove_cash(dictionary, cash):
+#     dictionary["admin"]["total_cash"] -= cash
+#     return get_total_cash
 
 # test 5 was straight forward, similar to test 1 & 2, but with different dict keywords
 def get_pets_sold(dictionary):
@@ -44,14 +44,16 @@ def increase_pets_sold(dictionary, sales):
 def get_stock_count(dictionary):
     return len(dictionary["pets"])
 
+# test 8 my first attemp at this wasn;t far off the mark, just needed to refer to 
+# others on slack to fine tune it,
 def get_pets_by_breed(dictionary, breed_type):
     breed_list = []
     for pet in dictionary["pets"]:
         if pet["breed"] == breed_type:
             breed_list.append(pet["breed"])
     return breed_list
-# my first attemp at this wasn;t far off the mark, just needed to refer to others on slack to fine tune it,
 
+# test 9 
 def get_pets_by_breed(dictionary, breed_type):
     dalmations = []
     for pet in dictionary["pets"]:
@@ -59,54 +61,51 @@ def get_pets_by_breed(dictionary, breed_type):
             dalmations.append(pet["breed"])
     return dalmations
 
+#10 why did this need a loop, and couldn't just be called 
+# same as the early functions i.e get_pet_shop_name
+# also i wanted my final return statement to be pet_name, 
+# how do i rearrange the function to end that way, e.g
 def find_pet_by_name(dictionary, pet_name):
     for pet in dictionary["pets"]:
         if pet["name"] == pet_name:
             return pet
 
-# why did this need a loop, and couldn't just be called same as the early functions i.e get_pet_shop_name
-# also i wanted my final return statement to be pet_name, how do i rearrange the function to end that way, e.g
-# def find_pet_by_name(dictionary, pet_name):
-#     for pet in dictionary["pets"]:
-#         if pet_name == pet["name"]:
-#             return pet_name
-
+#11 so basically the loop is doing the same as previous, but 
+# as it is not able to find the pet named "Fred", it defaults
+# with None?
 def find_pet_by_name(dictionary, pet_name):
     for pet in dictionary["pets"]:
         if pet["name"] == pet_name:
             return pet
 
-# so basically the loop is doing the same as previous, but as it is not able to find the pet named "Fred", it defaults with None?
-
+#12 found it hard to put the combo of words in right working order
 def remove_pet_by_name(dictionary, pet_remove):
     for pet in dictionary["pets"]:
         if pet["name"] == pet_remove:
             dictionary["pets"].remove(pet)
 
-# found it hard to put the combo of words in right working order for line 66
-
+# 13 referred to the example John ran through on Friday in the recap.
+# Wasn't sure if I was accessing the new_pet dictionary properly
+# before adding to the "pets" dictionary, or not. Ran the test
+# and it accepted so moved on.
 def add_pet_to_stock(dictionary, new_pet):
     dictionary["pets"].append(new_pet)
 
-# referred to the example John ran through on Friday in the recap.  Wasn't sure if I was accessing the new_pet dictionary properly before adding to the "pets" dictionary, or not. Ran the test and it accepted so moved on.
-
+#14
 def get_customer_cash(dictionary):
     return dictionary["cash"]
 
+#15 # the way the test is worded and set out I thought 
+# I might need to over complicate this one, was surprised 
+# that an easy solution like the early tests was able to make this pass.
 def remove_customer_cash(dictionary, cash):
     dictionary["cash"] -= 100
     return remove_customer_cash
 
-# the way the test is worded and set out I thought I might need to over complicate this one, was surprised that an easy solution like the early tests was able to make this pass.
+#16
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
 
-# def customer_pet_count(dictionary, count):
-#     return len(dictionary["pets"])
-
-# Couldnt get this function to work, brain also stopped working.
-
-# NameError: name 'get_customer_pet_count' is not defined - this was a regular error that I came across.
-
-# Couldnt get this function to work, brain also stopped working.
-
-# def add_pet_to_customer(customer, new_pet):
-#     customer["pets"].append(new_pet)
+#17
+def add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
